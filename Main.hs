1f990d8af330faaa66882161a1a4f8fd9cbe6787
Roman Dubrovsky <roma2107946@gmail.com>
2015-12-15 16:51:45 +0300

    Change testing

--- Main.hs
+++ Main.hs
@@ -21,7 +21,7 @@ type Property = [Double]
 type PropertiesCollection = M.Map Int Property
 type ClasifyCollection = M.Map String PropertiesCollection
 
-type ClassProperty = (Double, Double)
+type ClassProperty = (Double, Double, Double)
 type ClassPropertyCollection = M.Map Int ClassProperty
 type Clasify = M.Map String ClassPropertyCollection
 
@@ -35,23 +35,27 @@ traningObjects :: [Object] -> ClasifyCollection
 traningObjects = M.map traningProperties . foldl merge M.empty
   where merge m (k, xs) = M.insertWith (++) k [xs] m
 
-calculateProperties :: Property -> ClassProperty
-calculateProperties prop = (avge, disp)
+calculateProperties :: Double ->Property -> ClassProperty
+calculateProperties all prop = (avge, disp, per)
   where avge = (sum prop) / size
         disp = 1.0 / (size - 1) * disp_summ
         disp_summ = sum $ map (**2) $ map (\x -> x - avge) prop
         size = fromIntegral $ length prop
+        per = size / all
 
 training :: [Object] -> Clasify
 training objs = M.map calculateClasses $ traningObjects objs
-  where calculateClasses = M.map (\x -> calculateProperties x)
+  where calculateClasses = M.map (\x -> calculateProperties size x)
+        size = fromIntegral $ length objs
 
 -- =====  bayes clasify testing =====
 
 findClass :: Clasify -> [Double] -> String
-findClass clasify obj = snd $ maximum $ map (\(x,y) -> (y,x)) $ M.toList $ M.map findCoffs clasify
-  where findCoffs prop_collection = product $ zipWith zipper obj $ M.elems prop_collection
-        zipper prop (avge, disp) = 1 / sqrt(2 * pi * disp) * exp ((-1) * (prop - avge) ** 2 / 2 / disp)
+findClass clasify obj = snd $ maximum $ map (\(x,y)  -> (y,x)) $ M.toList $ M.map findCoffs clasify
+  where findCoffs prop_collection = (*) (persent prop_collection) $ product $ zipWith zipper obj $ M.elems prop_collection
+        zipper prop (avge, disp, _) = 1 / sqrt(2 * pi * disp) * exp ((-1) * (prop - avge) ** 2 / 2 / disp)
+        persent prop_collection = percentFor $ head $ M.elems prop_collection
+        percentFor (_, _, x) = x
 
 testingObject :: Clasify -> Object -> Bool
 testingObject clasify (clas, prop) = clas == findClass clasify prop
